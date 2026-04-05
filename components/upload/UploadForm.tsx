90b05422a3a5b7b2ce1ff00508a1685f93fdf310
faisal-din <dahrifaisal56@gmail.com>
2026-04-05 23:31:44 +0500

    Refactor UploadForm to integrate upload functionality and improve error handling

--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -3,17 +3,36 @@
 import { fileUploadSchema } from '@/constants/schema';
 import UploadFormInput from './UploadFormInput';
 import { toast } from 'sonner';
+import { useUploadThing } from '@/utils/uploadthing';
 
 const UploadForm = () => {
+  const { startUpload, routeConfig } = useUploadThing('pdfUploader', {
+    onClientUploadComplete: () => {
+      toast.success('File uploaded successfully!', {
+        style: {
+          backgroundColor: '#34d399',
+          color: '#fff',
+        },
+      });
+    },
+    onUploadError: (error) => {
+      toast.error('Error occurred while uploading.', {
+        style: {
+          backgroundColor: '#f87171',
+          color: '#fff',
+        },
+      });
+      console.log('Error occured while uploading file: ', error);
+    },
+    onUploadBegin: ({ file }) => {
+      console.log('upload has begun for', file);
+    },
+  });
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    toast.success('File uploaded successfully.', {
-      style: {
-        backgroundColor: '#34d399',
-        color: '#fff',
-      },
-    });
+    console.log('File uploaded successfully.');
 
     const formData = new FormData(e.currentTarget);
     const file = formData.get('file') as File;
@@ -24,35 +43,49 @@ const UploadForm = () => {
 
     if (!validatedFile.success) {
       // schema validation with zod
-      toast.error(
-        validatedFile.error.flatten().fieldErrors.file?.[0] || 'Invalid file.',
-        {
-          description: 'Please upload a valid PDF file.',
-          style: {
-            backgroundColor: '#f87171',
-            color: '#fff',
-          },
-        }
-      );
-      return;
-    }
-
-    // upload the file to the uploadthing
-    // parse the pdf using langchain
-    // summarize the content using AI
-    // save the summary to the database
-    // redirect to the summary page
-
-    if (file.type !== 'application/pdf') {
-      toast.warning('Please upload a valid PDF file.', {
-        description: 'Only PDF files are allowed.',
+      toast.error('Something went wrong', {
+        description:
+          validatedFile.error.flatten().fieldErrors.file?.[0] ||
+          'Invalid file.',
         style: {
-          backgroundColor: '#fbbf24',
+          backgroundColor: '#f87171',
           color: '#fff',
         },
       });
       return;
     }
+
+    toast('Uploading file...', {
+      description:
+        'Uploading your file to our servers. This may take a moment.',
+    });
+
+    // upload the file to the uploadthing
+    const uploadResponse = startUpload([file]);
+
+    console.log(
+      'Upload Response: ',
+      uploadResponse.then((res) => console.log('Upload Response: ', res))
+    );
+
+    if (!uploadResponse) {
+      toast.error('Something went wrong.', {
+        description: 'Failed to upload the file. Please use different file.',
+        style: {
+          backgroundColor: '#f87171',
+          color: '#fff',
+        },
+      });
+      return;
+    }
+
+    // toast('Processing file...', {
+    //   description: 'Hang tight! Our AI is reading through your document.',
+    // });
+    // parse the pdf using langchain
+    // summarize the content using AI
+    // save the summary to the database
+    // redirect to the summary page
   };
 
   return (
